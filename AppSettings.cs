aab3deaeb96a89c5885460e5df2b081847718775
payalpawar123 <36138108+payalpawar123@users.noreply.github.com>
2021-01-16 14:41:25 -0800

    Backend

---
+++ AppSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace StocksApp.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+        public string FirebaseUrl { get; set; }
+        public string FirebaseSecret { get; set; }
+    }
+}
